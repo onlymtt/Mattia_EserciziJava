9bc0ff27d61899eeb21fd501fd2150f2b3a83af1
onlymtt <162594554+onlymtt@users.noreply.github.com>
2024-03-11 14:14:30 +0100

    Update EsercizioMatematica.java
    Aggiunti commenti al codice

--- 11 marzo/EsercizioMatematica.java
+++ 11 marzo/EsercizioMatematica.java
@@ -3,28 +3,34 @@ import java.util.Scanner;
 public class EsercizioMatematica {
     public static void main(String[] args) {
         
+        //scanner per gli input
         Scanner scint = new Scanner(System.in);
         Scanner sc = new Scanner(System.in);
         int input = 0;
         String inputStr;
+        //totale delle varie operazioni
         int tot;
 
         int x = 3;
         int y = 8;
 
 
+            
             for (int i = 0; i >= 0;) {
 
                 System.out.println("\nVuoi calcolare? ");
                 inputStr = sc.nextLine();
+                //controllo se l'utente vuole calcolare o meno
                 if (inputStr.equalsIgnoreCase("no")) {
                     System.out.println("Hai calcolato " + i + " volte");
                     break;
                 } else if (inputStr.equalsIgnoreCase("sì")) {
                 
+                //menù con la scelta delle operazioni
                 System.out.println("Digita 1 per la somma, 2 per la sottrazione, 3 per il prodotto, 4 per il quoziente, qualsiasi altro numero per uscire\n");
                 input = scint.nextInt();
-
+            
+            //scelta delle varie operazioni
             switch (input) {
                 case 1:
                     tot = x + y;
@@ -35,7 +41,6 @@ public class EsercizioMatematica {
                     tot = x - y;
                     System.out.println("La sottrazione vale: " + tot);
                     break;
-
 
                 case 3:
                     tot = x * y;
@@ -52,12 +57,14 @@ public class EsercizioMatematica {
             }
 
 
-            } i++;
+            } i++; //incremento il contatore alla fine del ciclo perchè questo mi conta le volte che ho calcolato
             System.out.println("Hai calcolato " + i + " volte");
         }
             
             
         
         
+    }
+} 
     }
 }
