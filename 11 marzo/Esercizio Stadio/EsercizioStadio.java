68712d6d26f34dc80320bb7cba633366cb57291a
onlymtt <162594554+onlymtt@users.noreply.github.com>
2024-03-11 17:15:01 +0100

    Update EsercizioStadio.java
    Totale incassi aggiunto

--- 11 marzo/Esercizio Stadio/EsercizioStadio.java
+++ 11 marzo/Esercizio Stadio/EsercizioStadio.java
@@ -1,19 +1,44 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class EsercizioStadio {
     public static void main(String[] args) {
 
         Random random = new Random();
-        
+        Scanner sc = new Scanner(System.in);
         
         ArrayList<Integer> spettatoriSabato = new ArrayList<>();
+        ArrayList<Integer> spettatoriDomenica = new ArrayList<>();
 
-        //Sabato
+        int totIncasso = 0;
+        //Incasso Sabato
         for (int i = 0; i< 11; i++) {
             int numSpettatoriSabato = random.nextInt(10001);
-            int incassoSabato = numSpettatoriSabato * 20;
             spettatoriSabato.add(numSpettatoriSabato);
+            int incassoSabato = numSpettatoriSabato * 20;
+            totIncasso +=incassoSabato;
+        }
+
+        //Incasso domenica
+        for (int i = 0; i< 11; i++) {
+            int numSpettatoriDomenica = random.nextInt(10001);
+            spettatoriDomenica.add(numSpettatoriDomenica);
+            int incassoDomenica = numSpettatoriDomenica * 15;
+            totIncasso +=incassoDomenica;
+        }
+
+
+        System.out.println("Scegli un'opzione: \n1.Incasso totale");
+        int input = sc.nextInt();
+
+        switch (input) {
+            case 1:
+                System.out.println("Incasso totale " + totIncasso);
+                break;
+        
+            default:
+                break;
         }
     }
 }
