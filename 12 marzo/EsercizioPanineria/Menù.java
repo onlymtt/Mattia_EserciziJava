63ab8ef39c1d70d8fc3397ce742793b59cfc88ee
onlymtt <162594554+onlymtt@users.noreply.github.com>
2024-03-12 17:34:08 +0100

    Scelta ingrediente Mattia 17:33
    Aggiunta possibilità di scegliere l'ingrediente, con visualizzazione del panino finale e calcolo costo totale

--- 12 marzo/EsercizioPanineria/Menù.java
+++ 12 marzo/EsercizioPanineria/Menù.java
@@ -6,20 +6,25 @@ public class Menù {
 
     public static void main(String[] args) {
 
-        PiattoSpeciale piattoSpeciale;
 
-      final ArrayList listaTipoPane = new ArrayList<>();
+     final ArrayList listaTipoPane = new ArrayList<>();
         listaTipoPane.add("semola");
         listaTipoPane.add("bianco");
         listaTipoPane.add("kamut");
 
-        String[] listaIngredienti = {"prosciutto", "formaggio", "salame"};
+        // String[] listaIngredienti = {"prosciutto", "formaggio", "salame"};
+
+        ArrayList listaIngredienti = new ArrayList<>();
+        listaIngredienti.add("prosciutto");
+        listaIngredienti.add("formaggio");
+        listaIngredienti.add("salame");
+
 
         double[] prezziPane = {1.5, 2.1, 3.2};
         double[] prezziIngredienti = {2.0, 0.6, 1.4};
 
         Scanner sc = new Scanner(System.in);
-        String input;
+        String input, inputIngr;
 
         int prezziIndex = 0;
         double prezzoTotale = 0;
@@ -37,14 +42,33 @@ public class Menù {
         //scelta base panino
         if (listaTipoPane.contains(input)) {
             System.out.println("Hai scelto " + input);
-            piattoSpeciale = new PiattoSpeciale(input, "prosciutto");
-            System.out.println("La base del tuo panino è " + piattoSpeciale.getTipoPane() + " con " + piattoSpeciale.getIngrediente());
-            prezzoTotale = prezziPane[listaTipoPane.indexOf(input)] + 2.1;
-            System.out.println("Il prezzo è " + prezzoTotale);
+           // piattoSpeciale = new PiattoSpeciale(input, "prosciutto",  );
+           // System.out.println("La base del tuo panino è " + piattoSpeciale.getTipoPane() + " con " + piattoSpeciale.getIngrediente());
+           // prezzoTotale = prezziPane[listaTipoPane.indexOf(input)] + 2.1;
+          //  System.out.println("Il prezzo è " + prezzoTotale);
         } else {
-            System.out.println("Questo ingrediente non è presente!");
+            System.out.println("Questa base non è presente!");
         }
 
+        //scelta ingrediente
+        System.out.println("Scegli un ingrediente");
+        
+        for (int i = 0; i<listaIngredienti.size(); i++) {
+            System.out.println(listaIngredienti.get(i));
+            
+        }
+        inputIngr = sc.nextLine();
+        if (listaIngredienti.contains(inputIngr)) {
+            System.out.println("Hai scelto " + inputIngr);
+        } else {
+            System.out.println("Questo ingrediente non è nella lista");
+        }
 
+        prezzoTotale = listaTipoPane.indexOf(input) + listaIngredienti.indexOf(inputIngr);
+        PiattoSpeciale piattoSpeciale = new PiattoSpeciale(input, inputIngr, prezzoTotale);
+        
+        System.out.println("La base del tuo panino è " + piattoSpeciale.getTipoPane() + " con " + piattoSpeciale.getIngrediente());
+        System.out.println("Il tuo panino costa: " + piattoSpeciale.getTotPrezzo());
+        
     }
 }
--- 12 marzo/EsercizioPanineria/PiattoSpeciale.java
+++ 12 marzo/EsercizioPanineria/PiattoSpeciale.java
@@ -1,11 +1,13 @@
 public class PiattoSpeciale {
     private String tipoPane;
     private String ingrediente;
+    private double totPrezzo;
     
 
-    public PiattoSpeciale (String tipoPane, String ingrediente) {
+    public PiattoSpeciale (String tipoPane, String ingrediente, double totPrezzo) {
         this.tipoPane = tipoPane;
         this.ingrediente = ingrediente;
+        this.totPrezzo = totPrezzo;
     }
 
 
@@ -17,11 +19,19 @@ public class PiattoSpeciale {
         return this.ingrediente;
     }
 
+    public double getTotPrezzo () {
+        return this.totPrezzo;
+    }
+
     public void setTipoPane(String tipoPane) {
         this.tipoPane = tipoPane;
     }
 
     public void setIngrediente (String ingrediente) {
         this.ingrediente = ingrediente;
+    }
+
+    public double calcolaTotale(double prezzoIngrediente) {
+        return this.totPrezzo += prezzoIngrediente;
     }
 }
