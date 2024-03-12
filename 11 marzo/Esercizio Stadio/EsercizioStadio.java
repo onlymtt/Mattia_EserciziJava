19b22b291ab86df364068565807f13ddad529cb8
onlymtt <162594554+onlymtt@users.noreply.github.com>
2024-03-12 12:18:02 +0100

    metodo calcolaIncasso
    Aggiunto metodo per calcolare l'incasso totale invece di avere due cicli for diversi nel main

--- 11 marzo/Esercizio Stadio/EsercizioStadio.java
+++ 11 marzo/Esercizio Stadio/EsercizioStadio.java
@@ -5,37 +5,47 @@ import java.util.Scanner;
 
 public class EsercizioStadio {
 
+    Random random = new Random();
+        
+    int id = 0;
+        
+    ArrayList<Integer> spettatoriSabato = new ArrayList<>();
+    ArrayList<Integer> spettatoriDomenica = new ArrayList<>();
+
+    int numWeekEnd = random.nextInt(4)+1;
+
+    int totIncasso = 0;
+
+    
+    
+    public void calcolaIncasso() {
+        for (int i = 0; i<= numWeekEnd; i++) {
+            int numSpettatoriSabato = random.nextInt(10001);
+            int numSpettatoriDomenica = random.nextInt(10001);
+
+            spettatoriSabato.add(numSpettatoriSabato);
+            spettatoriDomenica.add(numSpettatoriDomenica);
+
+            int incassoSabato = numSpettatoriSabato * 20;
+            int incassoDomenica = numSpettatoriDomenica * 15;
+
+            totIncasso +=incassoSabato + incassoDomenica;
+            id = numSpettatoriSabato - 1;
+        }
+    }
+
     
     public static void main(String[] args) {
 
-        Random random = new Random();
         Scanner sc = new Scanner(System.in);
         int input;
-        int id = 0;
+
+        EsercizioStadio stadio = new EsercizioStadio();
         
-        ArrayList<Integer> spettatoriSabato = new ArrayList<>();
-        ArrayList<Integer> spettatoriDomenica = new ArrayList<>();
+        //Incasso Sabato e Domenica
+        stadio.calcolaIncasso();
 
-        int numWeekEnd = random.nextInt(4)+1;
-
-        int totIncasso = 0;
-        //Incasso Sabato
-        for (int i = 0; i<= numWeekEnd; i++) {
-            int numSpettatoriSabato = random.nextInt(10001);
-            spettatoriSabato.add(numSpettatoriSabato);
-            int incassoSabato = numSpettatoriSabato * 20;
-            totIncasso +=incassoSabato;
-            id = numSpettatoriSabato - 1;
-        }
-
-        //Incasso domenica
-        for (int i = 0; i< numWeekEnd; i++) {
-            int numSpettatoriDomenica = random.nextInt(10001);
-            spettatoriDomenica.add(numSpettatoriDomenica);
-            int incassoDomenica = numSpettatoriDomenica * 15;
-            totIncasso +=incassoDomenica;
-            id = numSpettatoriDomenica -1;
-        }
+        
         //Menù con le varie scelte
         do {
             System.out.println("\nScegli un'opzione: \n1. Incasso totale \n2. Minimo spettatori \n3. Massimo spettatori \n0. Per uscire");
@@ -45,22 +55,22 @@ public class EsercizioStadio {
                 //Incassi totali
                 case 1:
 
-                    System.out.println("Incasso totale " + totIncasso);
+                    System.out.println("Incasso totale " + stadio.totIncasso);
                     break;
 
                 case 2:
                     //minimo numero di spettatori
-                    int minSpettatori = Collections.min(spettatoriSabato);
+                    int minSpettatori = Collections.min(stadio.spettatoriSabato);
                     System.out.println("Il minimo degli spettatori del sabato è " + minSpettatori);
-                    minSpettatori = Collections.min(spettatoriDomenica);
+                    minSpettatori = Collections.min(stadio.spettatoriDomenica);
                     System.out.println("Il minimo degli spettatori della domenica è " + minSpettatori);
                     break;
 
                 case 3:
                     //massimo numero di spettatori
-                    int maxSpettatori = Collections.max(spettatoriSabato);
+                    int maxSpettatori = Collections.max(stadio.spettatoriSabato);
                     System.out.println("Il massimo degli spettatori del sabato è " + maxSpettatori);
-                    maxSpettatori = Collections.max(spettatoriDomenica);
+                    maxSpettatori = Collections.max(stadio.spettatoriDomenica);
                     System.out.println("Il massimo degli spettatori della domenica è " + maxSpettatori);
                     break;
             
