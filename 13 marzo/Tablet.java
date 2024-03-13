4b678225424de66ab126df8fd8d8c1e73ad8a4f7
onlymtt <162594554+onlymtt@users.noreply.github.com>
2024-03-13 16:34:29 +0100

    Classe Tablet
    Creazione classe Tablet con override di avviaApplicazione e valorizzazione di punti e id

--- 13 marzo/Tablet.java
+++ 13 marzo/Tablet.java
@@ -1,3 +1,23 @@
+import java.util.Random;
+
 public class Tablet extends DispositivoElettronico {
+    private int punti;
+    private int id = 0;
+
+    public Tablet(int punti, int id) {
+    Random random = new Random();
+    this.punti = random.nextInt(101);
+    this.id++;
+    }
+
+
+    @Override
+    public void avviaApplicazione(String nomeApp) {
+        System.out.println("L'applicazione " + nomeApp + " si sta avviando dal tablet");
+
+    }
+
     
+
+
 }
