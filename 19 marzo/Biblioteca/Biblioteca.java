30c97b5d4c889502d5d2b99ee1e19753cd20fefa
onlymtt <162594554+onlymtt@users.noreply.github.com>
2024-03-19 15:17:29 +0100

    Iterator
    Implementato iterator nel metodo rimuoviLibro

--- 19 marzo/Biblioteca/Biblioteca.java
+++ 19 marzo/Biblioteca/Biblioteca.java
@@ -1,15 +1,14 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Biblioteca {
     private ArrayList<Libro> elencoLibri = new ArrayList<>();
-    
 
-    public Biblioteca () {
+    public Biblioteca() {
 
     }
 
-    
     public void setElencoLibri(Libro libro) {
         elencoLibri.add(libro);
     }
@@ -27,52 +26,47 @@ public class Biblioteca {
         return false;
     }
 
-
     public void aggiungiLibro() {
         System.out.println("Aggiungi un libro");
         System.out.println("Come si chiama questo libro?");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        
-
         if (getNomeLibro(input) == false) {
 
-        System.out.println("Quante copie?");
-        int input1 = sc.nextInt();
-        //zero copie prestate perchè è appena stato aggiunto
-        Libro libro = new Libro(input, input1, 0);
-        this.elencoLibri.add(libro); 
-    }   else {
-        System.out.println("Libro già presente!");
-    }
-            
+            System.out.println("Quante copie?");
+            int input1 = sc.nextInt();
+            // zero copie prestate perchè è appena stato aggiunto
+            Libro libro = new Libro(input, input1, 0);
+            this.elencoLibri.add(libro);
+        } else {
+            System.out.println("Libro già presente!");
+        }
+
         stampaLibriDisponibili();
 
     }
 
-
-    //metodo per rimuovere un libro dalla collezione
+    // metodo per rimuovere un libro dalla collezione
     public void rimuoviLibro() {
         System.out.println("Rimuovi un libro");
         System.out.println("Come si chiama il libro?");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        Iterator<Libro> iterator = elencoLibri.iterator();
 
-        
-            
-                
-                for (Libro libro : elencoLibri) {
-                    if (getNomeLibro(input)) {
-                        elencoLibri.remove(libro);
-                }
+       while (iterator.hasNext()) {
+        Libro libroRimosso = iterator.next();
+            if (getNomeLibro(input)) {
+                iterator.remove();
             }
-        
+        }
+
         stampaLibriDisponibili();
-        
+
     }
 
-    //metodo per stampare tutta la lista di libri disponibili
+    // metodo per stampare tutta la lista di libri disponibili
     public void stampaLibriDisponibili() {
         System.out.println("I libri disponibili sono\n");
         for (Libro libro : elencoLibri) {
@@ -80,5 +74,4 @@ public class Biblioteca {
         }
     }
 
-} 
-
+}
