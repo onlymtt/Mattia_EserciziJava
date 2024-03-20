7c2ad18912499e7bb87945a82cb73a1b063240a5
onlymtt <162594554+onlymtt@users.noreply.github.com>
2024-03-20 10:09:20 +0100

    Conversione decimale binario
    Esercizio sulla conversione da decimale a binario

---
+++ 20 marzo/ConversioneDecBin/src/Conversione/Conversione.java
@@ -0,0 +1,30 @@
+package Conversione;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Conversione {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int resto;
+		String binary;
+		StringBuilder builder = new StringBuilder();
+        System.out.print("Inserisci un numero decimale: ");
+        int input = scanner.nextInt();
+        
+        
+        do {
+        resto = input % 2;
+        
+        input = input / 2;;
+        
+        builder.insert(0, resto);
+        
+        }while(input > 0);
+        
+        binary = builder.toString();
+        System.out.println("Il binario è " + binary);
+	}
+
+}
