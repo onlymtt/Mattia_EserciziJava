bc7d59e8915dcf87033e48fbc30ea32b792ff5b3
onlymtt <162594554+onlymtt@users.noreply.github.com>
2024-03-22 11:16:26 +0100

    Esercizio calcolo Area triangolo
    Esercizio calcolo area triangolo da file e da un array

---
+++ 22 marzo/AreaTriangolo/src/area_triangolo/Triangolo.java
@@ -0,0 +1,48 @@
+package area_triangolo;
+
+public class Triangolo {
+	private int base;
+	private int altezza;
+	private int area;
+	
+	
+	public Triangolo(int base, int altezza) {
+		super();
+		this.base = base;
+		this.altezza = altezza;
+		//this.area = area;
+	}
+
+
+	public int getBase() {
+		return base;
+	}
+
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+
+	public int getAltezza() {
+		return altezza;
+	}
+
+
+	public void setAltezza(int altezza) {
+		this.altezza = altezza;
+	}
+
+
+	public int getArea() {
+		return area;
+	}
+
+
+	public void setArea(int area) {
+		this.area = area;
+	}
+	
+	
+	
+}
